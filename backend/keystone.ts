ccd200acb90b15be0cde4fb5cc4ce8560597511a
Nursultan <nursultan@MacBook-Pro-Nursultan.local>
2023-03-07 23:39:11 +0500

    module #11

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -14,6 +14,7 @@ import { CartItem } from './schemas/CartItem';
 import { extendGraphqlSchema } from './mutations';
 import { OrderItem } from './schemas/OrderItem';
 import { Order } from './schemas/Order';
+import { Role } from './schemas/Role';
 
 const databaseUrl =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -62,6 +63,7 @@ export default withAuth(
       CartItem,
       OrderItem,
       Order,
+      Role,
     }),
     extendGraphqlSchema,
     ui: {
